b639a881866c2d973b1b3552388692dbf265b9ea
qingqingqingli <qli@DESKTOP-VVDRM16.localdomain>
2020-04-09 10:22:20 +0200

    fixed dda algorithm

--- 7_dda.c
+++ 7_dda.c
@@ -6,7 +6,7 @@
 /*   By: qli <qli@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/04/06 19:06:55 by qli           #+#    #+#                 */
-/*   Updated: 2020/04/08 20:35:32 by qli           ########   odam.nl         */
+/*   Updated: 2020/04/09 10:21:35 by qli           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,20 +46,8 @@ void 	ft_calculate_side_dist(t_input *input)
 /* len of ray from one x / y side to next x / y side */
 void 	ft_calculate_delta(t_input *input)
 {
-	input->dda.delta_dis_x = sqrt(1 + (ray_dir_y * ray_dir_y) /
-	(ray_dir_x * ray_dir_x));
-	input->dda.delta_dis_y = sqrt(1 + (ray_dir_x * ray_dir_x) /
-	(ray_dir_y * ray_dir_y));
-	// printf("1**input->dda.delta_dis_x is %f\n", input->dda.delta_dis_x);
-	// printf("1**input->dda.delta_dis_y is %f\n", input->dda.delta_dis_y);
-	// input->dda.delta_dis_x = abs(1 / ray_dir_x);
-	// input->dda.delta_dis_y = abs(1 / ray_dir_y);
-	// printf("2**input->dda.delta_dis_x is %f\n", input->dda.delta_dis_x);
-	// printf("2**input->dda.delta_dis_y is %f\n", input->dda.delta_dis_y);
-	// input->dda.delta_dis_x = (ray_dir_y == 0) ? 0 :	((ray_dir_x == 0) ? 1 : abs(1 / ray_dir_x));
-	// input->dda.delta_dis_y = (ray_dir_x == 0) ? 0 :	((ray_dir_y == 0) ? 1 : abs(1 / ray_dir_y));
-	// printf("3**input->dda.delta_dis_x is %f\n", input->dda.delta_dis_x);
-	// printf("3**input->dda.delta_dis_y is %f\n", input->dda.delta_dis_y);
+	input->dda.delta_dis_x = fabs(1 / ray_dir_x);
+	input->dda.delta_dis_y = fabs(1 / ray_dir_y);
 }
 
 /* in which box of the map we are in */
@@ -83,22 +71,23 @@ int 	ft_perform_dda(t_input *input)
 			input->dda.side_dis_x = input->dda.side_dis_x + input->dda.delta_dis_x;
 			input->dda.map_x = input->dda.map_x + input->dda.step_x;
 			input->dda.side = 0;
+			// printf("input->dda.map_x is %d\n", input->dda.map_x);
 		}
 		else
 		{
 			input->dda.side_dis_y = input->dda.side_dis_y + input->dda.delta_dis_y;
 			input->dda.map_y = input->dda.map_y + input->dda.step_y;
 			input->dda.side = 1;
+			// printf("input->dda.map_y is %d\n", input->dda.map_y);
 		}
 		/* check if ray has hit a wall */
-		if (input->array[input->dda.map_y][input->dda.map_x] == 1)
+		if (input->array[input->dda.map_y][input->dda.map_x] == '1')
 		{
-			printf("i am here\n");
 			hit = 1;
+			printf("input->dda.map_x is %d\n", input->dda.map_x);
+			printf("input->dda.map_y is %d\n", input->dda.map_y);
 		}
 	}
-	printf("input->dda.side is %d\n", input->dda.side);
-	printf("hit is %d\n", hit);
 	return (0);
 }
 
@@ -122,7 +111,7 @@ int 	ft_dda(t_input *input)
 	ft_calculate_delta(input);
 	ft_calculate_side_dist(input);
 	/* need to read all the calculations again before going to the next step */
-	// ft_perform_dda(input);
+	ft_perform_dda(input);
 	ft_calculate_ray_len(input);
 	return (0);
 }
