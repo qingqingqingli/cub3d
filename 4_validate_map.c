726b8cfdbd370d93537d75bf2d5ccdb4e62cffb4
qingqingqingli <qli@DESKTOP-VVDRM16.localdomain>
2020-05-04 10:30:12 +0200

    resolve another conflict

--- 4_validate_map.c
+++ 4_validate_map.c
@@ -6,7 +6,7 @@
 /*   By: qli <qli@student.codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/05/03 17:25:29 by qli           #+#    #+#                 */
-/*   Updated: 2020/05/04 10:28:05 by qli           ########   odam.nl         */
+/*   Updated: 2020/05/04 10:29:53 by qli           ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,14 +68,6 @@ int				ft_validate_map(t_input *input)
 		return (ft_return_error("Error\nmap error\n", input));
 	if (ft_find_player_position(input) == 0)
 		return (ft_return_error("Error\nNo player\n", input));
-<<<<<<< HEAD
-	// ft_duplicate_array(input, 0, 0);
-	// ft_check_top_border(input);
-	// ft_check_bottom_border(input);
-	// ft_flood_fill(input, input->flood_fill_x, input->flood_fill_y);
-	// // ft_print_2d_array(input->array_copy);
-	// free(input->array_copy);
-=======
 	if (ft_check_double_player(input) == -1)
 		return (ft_return_error("Error\nDouble player\n", input));
 	ft_check_top_border(input);
@@ -85,6 +77,5 @@ int				ft_validate_map(t_input *input)
 	ft_print_2d_array(input->array_copy);
 	ft_flood_fill(input, input->flood_fill_x, input->flood_fill_y);
 	free(input->array_copy);
->>>>>>> dda
 	return (0);
 }
